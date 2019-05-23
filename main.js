5c0d905a8ed2f4f562f4b08802f7827fd50c7a01
Nikolas Haug <nikolas.haug@futurenet.com>
2019-05-23 14:23:32 -0500

    add horizontal slide in

--- main.js
+++ main.js
@@ -8,8 +8,23 @@
 // });
 
 const parallax = document.querySelectorAll(".parallax");
+const horParallax = document.getElementById('fixed');
 window.addEventListener("scroll", function () {
     let offset = window.pageYOffset;
+    console.log(offset);
+    if(offset > 80) {
+        // horParallax.style.left = offset + 'px';
+        // horParallax.style.transform = `translate(${offset}px, 0)`;
+        // horParallax.style.opacity = 1;
+        horParallax.classList.add('active');
+    } else {
+        // horParallax.style.transform = 'translate(-100%, 0)';
+        // horParallax.style.opacity = 0;
+        horParallax.classList.remove('active');
+    }
+
+    // horParallax.style.left = offset + 'px';
+
     parallax.forEach(function (prllx, i) {
         console.log("Parallax " + i + ": " + prllx.offsetTop + " / Window: " + offset);
         prllx.style.backgroundPositionY = (offset - prllx.offsetTop) * 0.7 + "px";
